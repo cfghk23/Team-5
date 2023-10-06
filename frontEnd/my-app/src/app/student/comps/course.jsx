ce31700f7f63dce9c211ac6db4163139fde1f166
jackyyp <chpoonal@connect.ust.hk>
2023-10-06 13:49:18 +0800

    added course card, updated student page

---
+++ frontEnd/my-app/src/app/student/comps/course.jsx
@@ -0,0 +1,67 @@
+import Image from "next/image";
+
+import { cn } from "@/lib/utils";
+import {
+  ContextMenu,
+  ContextMenuContent,
+  ContextMenuItem,
+  ContextMenuSeparator,
+  ContextMenuSub,
+  ContextMenuSubContent,
+  ContextMenuSubTrigger,
+  ContextMenuTrigger,
+} from "@/components/ui/context-menu";
+
+import { Progress } from "@/components/ui/progress";
+
+// interface AlbumArtworkProps extends React.HTMLAttributes<HTMLDivElement> {
+//   album: Album
+//   aspectRatio?: "portrait" | "square"
+//   width?: number
+//   height?: number
+// }
+
+// destructure the props directly!!
+export function Course({
+  course,
+  aspectRatio = "portrait",
+  width,
+  height,
+  className,
+  ...props
+}) {
+  return (
+    <div className={cn("space-y-3", className)} {...props}>
+      <ContextMenu>
+        {/* use photo as right-click trigger */}
+        <ContextMenuTrigger>
+          <div className="overflow-hidden rounded-md">
+            {/* unsplash photo */}
+            <Image
+              src={course.image}
+              alt={course.name}
+              width={width}
+              height={height}
+              className={cn(
+                "h-auto w-auto object-cover transition-all hover:scale-105",
+                aspectRatio === "portrait" ? "aspect-[3/4]" : "aspect-square"
+              )}
+            />
+          </div>
+        </ContextMenuTrigger>
+
+        {/* panel after right click */}
+        <ContextMenuContent className="w-40">
+          <ContextMenuItem>Add to Library</ContextMenuItem>
+        </ContextMenuContent>
+      </ContextMenu>
+
+      {/* name under the photo */}
+      <div className="space-y-1 text-sm">
+        <Progress value={33} />
+        <span> 33% done</span>
+        <h3 className="font-medium leading-none">{course.name}</h3>
+      </div>
+    </div>
+  );
+}
--- frontEnd/my-app/src/app/student/page.js
+++ frontEnd/my-app/src/app/student/page.js
@@ -1,8 +1,99 @@
 import { StudentNavBar } from "./comps/studentNavBar";
+import { Button } from "@/components/ui/button";
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card";
+import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
+import { Separator } from "@/components/ui/separator";
+import { Course } from "./comps/course";
+import { Image } from "next/image";
+
 
 export default function Student() {
-    return (
-        <StudentNavBar />
+    const courses = [{
+        name: "course1",
+        iamge:
+            "https://images.unsplash.com/photo-1543002588-bfa74002ed7e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2730&q=80",
+    }];
 
+
+    return (
+        <>
+            <StudentNavBar />
+            <div className="grid grid-cols-2">
+                <Card>
+                    <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                        <CardTitle className="text-2xl font-medium">
+                            Challenge Question of the day
+                        </CardTitle>
+
+                    </CardHeader>
+                    <CardContent>
+                        <div className="flex flex-row">
+
+                            <div className="text-l font-bold">balloon popping</div>
+                            <Button className="ml-auto">Try</Button>
+                        </div>
+                    </CardContent>
+                </Card>
+
+                <Card>
+                    <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                        <CardTitle className="text-2xl font-medium">
+                            Notification from teacher
+                        </CardTitle>
+
+                    </CardHeader>
+                    <CardContent>
+                        <div className="text-l font-bold">Kahoot competition next week</div>
+                        <p className="text-xs text-muted-foreground">
+                            9/10/2023
+                        </p>
+                    </CardContent>
+                </Card>
+            </div>
+
+            <div className="grid grid-cols-2">
+                <div className="flex items-center justify-between">
+                    <div className="space-y-1">
+                        <h2 className="text-2xl font-semibold tracking-tight">
+                            Continue with your lesson
+                        </h2>
+                        <p className="text-sm text-muted-foreground">
+                            School ABC
+                        </p>
+                    </div>
+                </div>
+
+                {/* create a horizontal line */}
+                <Separator className="my-4" />
+
+                <div className="relative">
+                    {/* allow horizontal scrolling when too much item */}
+                    <ScrollArea>
+                        <div className="flex space-x-4 pb-4">
+                            {courses.map((course) => (
+                                <Course
+                                    key={course.name}
+                                    course={course}
+                                    className="w-[250px]"
+                                    aspectRatio="portrait"
+                                    width={250}
+                                    height={330}
+                                />
+                            ))}
+                        </div>
+                        <ScrollBar orientation="horizontal" />
+                    </ScrollArea>
+                </div>
+
+
+
+            </div>
+        </>
     )
 }
